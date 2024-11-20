9823b2e3766e6ad209a239262e1711a8864dfeaf
antoinedemathelin <antoine.demat@gmail.com>
2024-11-20 11:07:28 +0100

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maxwent",
-    version="0.1.0",
+    version="0.2.0",
     description="Maximum Weight Entropy",
     author="Antoine de Mathelin",
     author_email="antoine.demat@gmail.com",
